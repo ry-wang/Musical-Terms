05e4c5ef31c2e1d7509b5149e7f8899289938b55
Ryan Wang <shirui.wang@hotmail.com>
2016-04-26 22:58:25 -0400

    Initial Commit

---
+++ Musical Terms/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Musical Terms
+//
+//  Created by Ryan Wang on 2016-04-26.
+//  Copyright © 2016 Ryan Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
